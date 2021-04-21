ebb943399fa398ccbae6d815b93bde4aa7815243
Pedro Pacheco <pacheckdev@gmail.com>
2021-04-21 00:00:03 -0300

    added header comp

--- src/components/header.js
+++ src/components/header.js
@@ -3,27 +3,51 @@ import Link from 'gatsby-link';
 import Logo from '../assets/logo-designcode.svg';
 
 import './header.css';
+class Header extends React.Component {
+  constructor(props){
+    super(props);
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={Logo} width="30" />
-      </Link>
-      <Link to="/courses">
-        Courses
-      </Link>
-      <Link to="/downloads">
-        Downloads
-      </Link>
-      <Link to="workshops">
-        Workshops
-      </Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </div>
-)
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset;
+    if(scrollTop > 50){
+      this.setState({ hasScrolled: true })
+    }else{
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render(){
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={Logo} width="30" />
+          </Link>
+          <Link to="/courses">
+            Courses
+          </Link>
+          <Link to="/downloads">
+            Downloads
+          </Link>
+          <Link to="workshops">
+            Workshops
+          </Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
